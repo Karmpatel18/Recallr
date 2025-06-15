d6596932089cc28570647214b3ccd1765e51df55
Karmpatel18 <karmpatel1203@gmail.com>
2025-06-15 22:54:35 +0530

    toastify notification add

--- recallr/src/App.tsx
+++ recallr/src/App.tsx
@@ -4,23 +4,37 @@ import { Dashboard } from './pages/Dashboard'
 import { Signup } from './pages/Signup'
 import { Signin } from './pages/Signin'
 import { ProtectedRoute } from './pages/ProtectedRoute'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 function App() {
 
   return (
 
     <div className='bg-secondary font-manrope flex w-full  min-h-screen'>
-        <Routes>
-          <Route path='/' element={<Home />} />
+      <Routes>
+        <Route path='/' element={<Home />} />
 
 
-          <Route path="/signup" element={<Signup />} />
-          <Route path='/signin' element={<Signin/>}/>
-          <Route
-            path="/dashboard"
-            element={<ProtectedRoute><Dashboard /></ProtectedRoute>}
-          />
-        </Routes>
-      </div>
+        <Route path="/signup" element={<Signup />} />
+        <Route path='/signin' element={<Signin />} />
+        <Route
+          path="/dashboard"
+          element={<ProtectedRoute><Dashboard /></ProtectedRoute>}
+        />
+
+      </Routes>
+      <ToastContainer 
+        position="top-right" 
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark" />
+    </div>
   )
 }
 
--- recallr/src/pages/Signin.tsx
+++ recallr/src/pages/Signin.tsx
@@ -1,67 +1,85 @@
 import { useNavigate } from "react-router-dom";
 import { Input } from "../components/ui/Input";
 import Button from "../components/ui/Button";
-import { useRef } from "react";
-
+import { useRef, useState, useEffect } from "react";
+import { toast } from 'react-toastify';
 
 export const Signin = () => {
     const usernameRef = useRef<HTMLInputElement>(null);
     const passwordRef = useRef<HTMLInputElement>(null);
+    
     const navigate = useNavigate();
+
     async function handleSignin() {
-        const username = usernameRef.current?.value;
-        const password = passwordRef.current?.value;
-        const response = await fetch(import.meta.env.VITE_BACKEND_API + "/signin", {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json', // tells the backend to expect JSON
-            },
-            body: JSON.stringify({
-                username: username,
-                password: password,
-            }),
-        })
-        const data = await response.json();
-        console.log(data.send)
-        if (response.ok) {
-            localStorage.setItem('token', data.token); // Store JWT token
-            console.log('Signed in successfully!');
-        } else {
-            console.error('Error:', data.message);
+        try {
+            const username = usernameRef.current?.value;
+            const password = passwordRef.current?.value;
+            const response = await fetch(import.meta.env.VITE_BACKEND_API + "/signin", {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json', // tells the backend to expect JSON
+                },
+                body: JSON.stringify({
+                    username: username,
+                    password: password,
+                }),
+            })
+            const data = await response.json();
+            console.log(data.send)
+            if (response.ok) {
+                toast.success("Sign-in successful!")
+                localStorage.setItem('token', data.token); // Store JWT token
+                console.log('Signed in successfully!');
+                
+            } else {
+                toast.error(data.message || "Something went wrong");
+                console.error('Error:', data.message);
+            }
+            navigate("/dashboard")
         }
-        navigate("/dashboard")
+
+        catch (err) {
+            console.error(err);
+            
+            toast.error("Server error");
+        }
+
+
     }
+    
+
     return (
         <div className="flex w-full min-h-screen justify-center items-center">
             <div className="flex flex-col w-full items-center justify-center">
-            <div className="flex flex-col max-w-md space-y-3 w-full p-4 bg-primary rounded-2xl border-neutral-200 border-[1px]">
-                <div className="font-semibold text-2xl tracking-tighter">
-                    Signin recallr*
+                <div className="flex flex-col max-w-md space-y-3 w-full p-4 bg-primary rounded-2xl border-neutral-200 border-[1px]">
+                    <div className="font-semibold text-2xl tracking-tighter">
+                        Signin recallr*
+                    </div>
+                    <Input
+                        ref={usernameRef}
+                        placeholder="username"
+                        label="username" />
+                    <Input
+                        ref={passwordRef}
+                        placeholder="password"
+                        label="password" />
+                    <Button
+                        onClick={handleSignin}
+                        text="Signin"
+                        variant="primary"
+                        size="md" />
                 </div>
-                <Input
-                    ref={usernameRef}
-                    placeholder="username"
-                    label="username" />
-                <Input
-                    ref={passwordRef}
-                    placeholder="password"
-                    label="password" />
-                <Button
-                    onClick={handleSignin}
-                    text="Signin"
-                    variant="primary"
-                    size="md" />
+                <div className="text-neutral-400 tracking-tighter mt-2 font-normal">Don't have an account
+                    <span
+                        onClick={() => {
+                            navigate("/signup")
+                        }}
+                        className="text-neutral-800 underline underline-offset-2 cursor-pointer ml-1.5">SignUp now</span>
+                </div>
+                
             </div>
-                <div className="text-neutral-400 tracking-tighter mt-2 font-normal">Don't have an account  
-                <span 
-                onClick={() => {
-                    navigate("/signup")
-                } }
-                className="text-neutral-800 underline underline-offset-2 cursor-pointer ml-1.5">SignUp now</span>
-            </div>
-            </div>
-            </div>
+        </div>
 
-        
+
     )
 }
